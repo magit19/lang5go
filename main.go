382ab140a4b530cd41b94791c340ab67bedbc6b7
Aleksandr Pavlov <7911562+av-pavlov@users.noreply.github.com>
2019-12-13 16:04:59 +0900

    static files

--- main.go
+++ main.go
@@ -20,6 +20,11 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	// Эти две строки включают выдачу статических файлов 
+	// из папки static по адресу http://localhost:5000/static
+	fs := http.FileServer(http.Dir("static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)
 }
